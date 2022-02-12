754f2b333ffea4c39e1d15b5555b573bb09d784e
TimurPuchinsky <timur.2.puchinsky@gmail.com>
2022-02-12 08:36:13 +0300

    LR01

---
+++ MyConsole/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MyLibrary;
+
+
+namespace MyConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Duck duck1 = new RedheadDuck();
+            Duck duck2 = new MallardDuck();
+
+            Duck[] mas = new Duck[] { duck1, duck2 };
+
+            for (int i = 0; i < mas.Length; i++)
+            {
+                Console.WriteLine(mas[i].Swim());
+                Console.WriteLine(mas[i].Display());
+                Console.WriteLine(mas[i].Quack());
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
